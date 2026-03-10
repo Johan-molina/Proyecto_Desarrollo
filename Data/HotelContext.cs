a18e245aa9bed8c50750c2d4727240ef747512e7
juanca527 <jkamilo501@gmail.com>
2026-03-09 21:02:58 -0500

    Add files via upload

---
+++ Data/HotelContext.cs
@@ -0,0 +1,10 @@
+﻿using HotelReservasAPI.Models;
+
+namespace HotelReservasAPI.Data
+{
+    public static class HotelContext
+    {
+        public static List<Reserva> Reservas = new List<Reserva>();
+        public static List<Cita> Citas = new List<Cita>();
+    }
+}
---
+++ Data/HotelDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelReservasAPI.Models;
+
+namespace HotelReservasAPI.Data
+{
+    public class HotelDbContext : DbContext
+    {
+        public HotelDbContext(DbContextOptions<HotelDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Cita> Citas { get; set; }
+    }
+}
